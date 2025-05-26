3e0cdccdd618ca4c67b40c3046eb57fe56da9bd9
Kiki <182134344+kikinjeri@users.noreply.github.com>
2025-05-26 11:04:03 -0400

    alert.tsx
    passing html through children properties
    react.reacNode
    RAFCE - react arrow function component export

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import Alert from './components/Alert';
+
+function App() {
+    return (
+        <div>
+           <Alert>
+            Hello <span>World</span>
+           </Alert>
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ Message.tsx
@@ -0,0 +1,13 @@
+// PascalCasing
+
+function Message(){
+
+
+    const name = 'Mwihaki';
+   
+    if (name)
+    return <h1>Hello {name}</h1>
+        return <h1>Hello World</h1>;
+}
+
+export default Message;
