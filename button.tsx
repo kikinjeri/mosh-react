63dbab525571b5d036110a797da1e91e2e8d0d43
Kiki <182134344+kikinjeri@users.noreply.github.com>
2025-05-27 15:07:19 -0400

    Add files via upload

--- button.tsx
+++ button.tsx
@@ -1,21 +1,28 @@
-import { Children } from "react";
-import { ReactNode } from "react";
+import styles from "./Button.module.css";
 
 interface Props {
-    children: string; 
-    color ?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
-    onClick: () => void;
+  children: string;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+  onClick: () => void;
 }
 
-const Button = ({ children, onClick, color = 'danger'}: Props) => {
-    return (
-        <button className={'btn btn-' + color} onClick={onClick}>
-        {children}
-      
-        </button>
-    )
-    }
-
-    
+const Button = ({ children, onClick, color = "danger" }: Props) => {
+  return (
+    <button
+      className={[styles.btn, styles["btn-" + color]].join(" ")}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
---
+++ hearticon.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+
+function HeartIcon () {
+  return (
+  <div>
+    <FaHeart color='red' size = '50'>
+
+    </FaHeart>
+  </div>
+  )
+}
+  
+
+export default HeartIcon;
